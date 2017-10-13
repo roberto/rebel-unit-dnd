5e063ca52422e9bf6a0f27481266a66ab74f840b
Roberto Soares <roberto.tech@gmail.com>
2017-10-13 19:46:13 -0300

    adds converted values, keeping original values

--- imperial-dnd.js
+++ imperial-dnd.js
@@ -1,4 +1,23 @@
+var feetToMeters = function (value) {
+  return (value * 0.3048).toString() + ' meters'
+}
+
+var convert = {
+  'feet': feetToMeters,
+  'foot': feetToMeters
+}
+
 findAndReplaceDOMText(document.getElementsByTagName('body')[0], {
-  find: /10 feet/g,
-  replace: '3 metros'
+  find: /(\d+) (feet|foot)/g,
+  replace: function (node, found) {
+    var original = {
+      text: found[0],
+      value: parseInt(found[1]),
+      unit: found[2]
+    }
+
+    var converted = convert[original.unit](original.value)
+
+    return original.text + ' (' + converted + ')'
+  }
 })
