2629e59ad7197856a18b4adedaaffd933ad4e1fa
Roberto Soares <roberto.tech@gmail.com>
2017-10-14 17:14:18 -0300

    converts (number)-foot-wide

--- content-script.js
+++ content-script.js
@@ -1,7 +1,20 @@
+var feetToMeters = function (feet) {
+  return Qty(feet).to('m').toPrec('0.5 m')
+}
+
 findAndReplaceDOMText(document.body, {
   find: /(\d+) (feet|foot|ft)/g,
   replace: function (portion) {
-    var converted = Qty(portion.text).to('m').toPrec('0.5 m')
+    var converted = feetToMeters(portion.text)
+
+    return portion.text + ' (' + converted + ')'
+  }
+})
+
+findAndReplaceDOMText(document.body, {
+  find: /(\d+)‐(foot)-wide/g,
+  replace: function (portion, match) {
+    var converted = feetToMeters(match[1] + match[2])
 
     return portion.text + ' (' + converted + ')'
   }
