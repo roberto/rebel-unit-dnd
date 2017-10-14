e8c850c5ebfed0685a41c9497b676bbc357976bb
Roberto Soares <roberto.tech@gmail.com>
2017-10-14 16:11:37 -0300

    adds js-quantities to convert measures

--- content-script.js
+++ content-script.js
@@ -1,23 +1,8 @@
-var feetToMeters = function (value) {
-  return (value * 0.3048).toString() + ' meters'
-}
+findAndReplaceDOMText(document.getElementById('content'), {
+  find: /(\d+) (feet|foot|ft)/g,
+  replace: function (node) {
+    var converted = Qty(node.text).to('m')
 
-var convert = {
-  'feet': feetToMeters,
-  'foot': feetToMeters
-}
-
-findAndReplaceDOMText(document.getElementsByTagName('body')[0], {
-  find: /(\d+) (feet|foot)/g,
-  replace: function (node, found) {
-    var original = {
-      text: found[0],
-      value: parseInt(found[1]),
-      unit: found[2]
-    }
-
-    var converted = convert[original.unit](original.value)
-
-    return original.text + ' (' + converted + ')'
+    return node.text + ' (' + converted + ')'
   }
 })
